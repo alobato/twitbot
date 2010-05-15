96ccd262a330c1cd0ef7a394584fa949ec259dd9
raphaeldealmeida <jaguarnet7@gmail.com>
2010-05-16 00:00:07 +0800

    metodo append criado

---
+++ bot.rb
@@ -0,0 +1,3 @@
+def append(twitt, original_user, secound_user)
+  "RT @#{original_user}: #{twitt}. O que voce acha, @#{secound_user}?"
+end
--- bot_spec.rb
+++ bot_spec.rb
@@ -1,7 +1,18 @@
 require 'bot'
 
-describe "Testing" do
-  it "testing test should" do
-    true.should be_true
+describe "Twitbot" do
+
+  
+  it "should append phrase on twitt" do
+
+    original_twitt = "Hoje acordei e fui a praia sozinha pensar na vida... \nFoi tao bom, vou fazer isso mais vezes"
+    original_user = 'jehverona'
+    
+    secound_user = 'viniciussbs'
+
+    twitt = append original_twitt, original_user, secound_user
+    twitt.should eql "RT @jehverona: Hoje acordei e fui a praia sozinha pensar na vida... \nFoi tao bom, vou fazer isso mais vezes. O que voce acha, @viniciussbs?"
+          
   end
+
 end
